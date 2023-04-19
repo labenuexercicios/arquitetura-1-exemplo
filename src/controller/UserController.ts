67a03894d8c84472db3770f7695171f3986088b4
Yuzo <yuzokamoto@gmail.com>
2023-04-19 09:51:23 -0300

    Update UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -18,7 +18,7 @@ export class UserController {
         } catch (error) {
             console.log(error)
     
-            if (req.statusCode === 200) {
+            if (res.statusCode === 200) {
                 res.status(500)
             }
     
@@ -46,7 +46,7 @@ export class UserController {
         } catch (error) {
             console.log(error)
     
-            if (req.statusCode === 200) {
+            if (res.statusCode === 200) {
                 res.status(500)
             }
     
